efb3316741cc3b18edc3ead422a99035a3984f7d
육영현 <yyh6066@tsway.co.kr>
2024-10-10 20:37:45 +0900

    라우터 설정 | 페이지 진입 전 세션확인 코드

--- src/router/index.js
+++ src/router/index.js
@@ -5,22 +5,26 @@ import CalendarPage from '../components/DemoApp.vue'
 import LoginForm from '@/components/LoginForm.vue'
 import SignUpForm from '@/components/SignUpForm.vue'
 import TaskManager from '@/components/TaskManager.vue'
+import store from '@/store/index.js'
 
 const routes = [
   {
     path: '/',
     name: 'HomeVue',
-    component: Home
+    component: Home,
+    meta: { auth: true }
   },
   {
     path: '/requested-box',
     name: 'RequestedBox',
-    component: RequestedBox
+    component: RequestedBox,
+    meta: { auth: true }
   },
   {
     path: '/calendar-page',
     name: 'CalendarPage',
-    component: CalendarPage
+    component: CalendarPage,
+    meta: { auth: true }
   },
   {
     path: '/login-form',
@@ -35,13 +39,29 @@ const routes = [
   {
     path: '/task-manager',
     name: 'TaskManager',
-    component: TaskManager
+    component: TaskManager,
+    meta: { auth: true }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach(async (to, from, next) => {
+  const userId = store.state?.user?.userId
+  console.log(userId)
+  if (to.matched.some((r) => r.meta.auth) && userId == null) {
+    alert('로그인이 필요한 페이지입니다!')
+    if (to.path !== '/login-form') {
+      next('/login-form')
+    } else {
+      next() // 로그인 페이지일 경우 그대로 진행
+    }
+  } else {
+    next() // 그 외의 경우는 정상적으로 진행
+  }
 })
 
 export default router
